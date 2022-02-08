f0281d590abd1cc7bf90645348cc77ba25e2fcf9
Wyq1104 <ywei37@dons.usfca.edu>
2022-02-07 23:58:41 -0800

    change text

--- src/App.js
+++ src/App.js
@@ -6,8 +6,44 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is Yiqi Wei's Profile page
         </p>
         <a
           className="App-link"
